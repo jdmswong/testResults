df3cfc1bce23d897c52fae91df76959cf68da47c
JD Wong <jdmswong@gmail.com>
2018-01-27 12:09:54 -0800

    spellfix

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ import q2 from './q2';
 import q3 from './q3';
 import q4 from './q4';
 
-const anserNumbers = [
+const answerNumbers = [
   { number: 1, func: q1 },
   { number: 2, func: q2 },
   { number: 3, func: q3 },
   { number: 4, func: q4 },
 ];
-anserNumbers.forEach(qObj => {
+answerNumbers.forEach(qObj => {
   console.log(`Question ${qObj.number}`);
   console.log(qObj.func(persons));
   console.log('');
